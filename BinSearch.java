cfbaf12187e594dfa97785707e6024c55cf60117
RaazzKihsore <raazzkishore@gmail.com>
2022-08-31 18:44:33 +0530

    First commit

---
+++ BinSearch.java
@@ -0,0 +1,30 @@
+public class BinSearch {
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        final long startTime = System.currentTimeMillis();
+        int d = binSearch(arr, 2);
+        final long duration = System.currentTimeMillis() - startTime;
+        System.out.println("Execution time:" + duration);
+        System.out.println(d);
+    }
+
+    public static int binSearch(int[] arr, int x) {
+        int low = 1;
+        int high = arr.length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] > x) {
+                high = mid - 1;
+
+            } else if (x > arr[mid]) {
+                low = mid + 1;
+            } else
+                return mid;
+
+        }
+
+        return 0;
+    }
+
+}
